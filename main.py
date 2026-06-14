e87bfd8fc9f569ecb24b0d578a9ad7277112c934
emilianorojas89 <emiliano.rojas.code@gmail.com>
2026-06-14 02:23:11 -0300

    Ajustar comentarios del código

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ RUTA_CSV = "data/paises.csv"
 
 def mostrar_menu():
 
-    # Mostramos las opciones principales del sistema
+    
     print("\nMENÚ PRINCIPAL")
     print("1. Listar todos los países")
     print("2. Agregar un país")
@@ -39,43 +39,40 @@ def main():
     # Cargamos los países desde el archivo CSV
     lista_paises = datos.cargar_paises(RUTA_CSV)
 
-    # Repite el menú hasta que el usuario decida salir
+    
     while True:
-        # Mostramos el menú principal
+        
         mostrar_menu()
 
         # Pedimos y validamos la opción ingresada
         opcion = validaciones.validar_opcion_menu(0, 7)
 
-        # Opción 1 - Mostrar todos los países
         if opcion == 1:
             datos.mostrar_paises(lista_paises)
 
-        # Opción 2 - Agregar un país
+        
         elif opcion == 2:
             abm_paises.agregar_pais(lista_paises, RUTA_CSV)
 
-        # Opción 3 - Actualizar un país existente
+
         elif opcion == 3:
             abm_paises.actualizar_pais(lista_paises, RUTA_CSV)
 
-        # Opción 4 - Buscar países por nombre
+
         elif opcion == 4:
             busquedas.buscar_pais(lista_paises)
 
-        # Opción 5 - Filtrar países
         elif opcion == 5:
             filtros.menu_filtros(lista_paises)
 
-        # Opción 6 - Ordenar países
+        
         elif opcion == 6:
             ordenamiento.menu_ordenamiento(lista_paises)
 
-        # Opción 7 - Mostrar estadísticas
         elif opcion == 7:
             estadisticas.mostrar_estadisticas(lista_paises)
 
-        # Opción 0 - Finalizar programa
+        
         elif opcion == 0:
             print("\nPrograma finalizado.")
             break
